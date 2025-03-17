c35cf91ef5af8b5ba06c944bcfa85e8185a15bf2
sourabhragh <SOURABH2003r@GMAIL.COM>
2025-03-17 06:24:13 +0000

    variable

---
+++ 01_basics/01variable.js
@@ -0,0 +1,17 @@
+const accountId =14453
+let accountEmail="hitesh@google.com"
+var accountpassword="12345"
+accountCity="jaipur"
+
+//accountId=2 //not allowed
+
+console.log(accountId);
+
+accountEmail="hc@12gmail.com"
+accountpassword="213213"
+accountcity="benguluru"
+let accountstate;
+/*
+prepare not to use,because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountpassword,accountcity,accountstate])
