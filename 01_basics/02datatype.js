3c137a8365644b21e318364fbc706cc47889044a
sourabhragh <SOURABH2003r@GMAIL.COM>
2025-03-17 06:21:05 +0000

    datatype

---
+++ 01_basics/02datatype.js
@@ -0,0 +1,27 @@
+"use strict";// treat all js code as newer version
+
+alert(3 + 3)// we are using nodejs not browser
+
+ console.log(3
+    +
+     3) // code readability shoud be high
+ 
+    console.log("sourabh");
+
+    let name="sourabh"
+    let age=20
+    let isloggedIn = false
+
+    //number=>2 to power 53
+    //bigint
+    //string=>""
+    //boolean=>true/false
+    //null    
+    //undefined=>
+    //symbol=>unique
+
+
+    //object
+
+    console.log(typeof undefined);// undefined
+    console.log(typeof null);//object    
