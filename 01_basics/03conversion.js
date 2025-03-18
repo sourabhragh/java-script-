a326bdf47d4c9cd35924f646e059d0dafac06ad4
sourabhragh <SOURABH2003r@GMAIL.COM>
2025-03-18 18:51:45 +0000

    conversion

---
+++ 01_basics/03conversion.js
@@ -0,0 +1,26 @@
+let score = null
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>nan
+//true=>1; "false=>0"
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=>true; 0=>false;
+//empty=>false
+//"hitesh"=>true
+let someNumber=33
+let stringNumber=string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
