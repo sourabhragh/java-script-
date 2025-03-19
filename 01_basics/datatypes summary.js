84f5b4c7f9de329fd4a756c9b473e2ce89b8f703
sourabhragh <SOURABH2003r@GMAIL.COM>
2025-03-19 06:40:57 +0000

    datatypes summary

---
+++ 01_basics/datatypes summary.js
@@ -0,0 +1,30 @@
+//premitive
+
+// 7 types:string,number,boolean,null,undefined,symbol,bigint
+
+//const score=100
+const scorevalue=100.3
+
+//const isLoggedIn = false
+//const outsidetemp=null
+//let userEmail;
+
+const id = symbol('123')
+const anotherId= symbol('123')
+
+console.log(id===anotherId);
+
+//reference type(non pre)
+//array,objects,functions
+
+const heros= ["shaktiman","naagraj","doga"]
+ let myobj={
+  name:"sourabh",
+  age:22
+ }
+ const myfunction=function(){
+    console.log("hello world");
+ } 
+   
+ console.log(typeof myfunction);
+ 
