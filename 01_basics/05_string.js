158b5c4db40bad176dff775281ef57a103b54899
sourabhragh <SOURABH2003r@GMAIL.COM>
2025-03-20 17:15:34 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,40 @@
+const name="hitesh"
+//const repocount=50
+
+//console.log(name + repocount + "value");
+
+//use this one
+//console.log(`Hello my name is ${name} and my repocount is ${repocount}`);
+
+const gamename = new String('sourabh-raguh-wanshi')
+
+//console.log(gamename[0]);
+//console.log(gamename.__proto__);
+
+
+ //console.log(gamename.length);
+ //console.log(gamename.touppercase());
+  console.log(gamename.charAt(2));
+  console.log(gamename.indexOf('r'));
+
+  const newstring = gamename.substring(0,4)
+  console.log(newstring);
+  
+  const anotherstring = gamename.slice(-8,4)
+  console.log(anotherstring);
+  
+  const newstringone = " sourabh "
+  console.log(newstringone);
+  console.log(newstringone.trim);
+   
+  const url = "https://sourabh.com/sourabh%20choudary"
+
+  console.log(url.replace('%20','-'));
+  
+  console.log(url.includes('sunder'));
+
+  console.log(gamename.split('-'));
+  
+  
+  
+ 
