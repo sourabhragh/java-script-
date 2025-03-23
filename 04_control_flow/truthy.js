c6458ade440ccb9ec12708f415becdf8e6ae5613
sourabhragh <SOURABH2003r@GMAIL.COM>
2025-03-23 19:11:37 +0000

    truthy

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,30 @@
+const useremail = []
+if(useremail) {
+    console.log("got user email");
+} else {
+    console.log("don't have user email");
+    
+}
+ //falsy values 
+ //  false,0,BigInt 0n, "",null,undefined,NaN
+
+ //TRUTHY VALUES
+  //"0",'false'," ",[],{},fuction(){}
+//   if (useremail.length===0) {
+//     console.log("array is empty");
+    
+    
+//   }
+//  const emptyobj = {}
+//  if (Object.keys(emptyobj).length===0) {
+//     console.log("object is empty");
+//  }  
+    
+ //Nullish coalescing operator(??):null undefined
+ let val1;
+ //val = 5??10
+ //val1 = null?? 10
+ // val1 = undefined?? 15
+
+ console.log(val1);
+  
