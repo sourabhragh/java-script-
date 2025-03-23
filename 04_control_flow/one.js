f95fbf5d97bdb5e3a6f4f7764088c51866b9fc0b
sourabhragh <SOURABH2003r@GMAIL.COM>
2025-03-23 18:42:43 +0000

    flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,52 @@
+//if 
+// const isuserlogged = ture
+// const temperature = 41
+
+//  if(temperature===40){
+//     console.log("less than 50");
+//  }
+//  else{
+//     console.log("temprature is greatert than 50");
+    
+// }
+//  console.log(execute);
+
+//    const score = 200
+//    if(score>100){
+//     const power = "fly"
+//     console.log(`user power:${power}`);
+    
+//    }
+// console.log(`user power:${power}`);
+ 
+    //const balance = 1000
+    //not a good practice
+    //if(balance>500) console.log("test"),console.log("test2");
+    
+    //   if(balance<500){
+    //     console.log("less than 500");}
+    //      else if(balance>700){
+    //         console.log("greater than 700");
+            
+    //      }   else if(balance <900){
+    //         console.log("less than 900");
+            
+    //      }  else{
+    //         console.log("less than 1200");
+            
+    //      }
+
+         const userloggedin = true
+         const debitcard = true
+         const loggedinfromgoogle = false
+         const loggedinfromemail = true
+         if(userloggedin && debitcard){
+            console.log("allow to buy course");
+            
+         }
+        if(loggedinfromgoog ||loggedinfromemail){
+            console.log("user logged in");
+            
+        }
+
+      
