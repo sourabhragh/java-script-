6023acdb0097cc531504ca85ec5e1d41d028f6f5
sourabhragh <SOURABH2003r@GMAIL.COM>
2025-03-24 06:19:50 +0000

    while

---
+++ 05_iteration/two.js
@@ -0,0 +1,13 @@
+//while
+let index = 0
+// while (index<=10) {
+//     console.log(`value of index is ${index}`);
+//       index = index + 2
+// }
+//  let myarray = ["flash","batman","superman"]
+//  let arr = 0
+//  while (arr < myarray.length) {
+//     console.log(`value is ${myarray[arr]}`);
+//     arr = arr + 1
+    
+//  }
