fc9b39cc0bb8eb67f3a1b73eecf0ab9a61dafe0d
sourabhragh <SOURABH2003r@GMAIL.COM>
2025-03-24 07:27:28 +0000

    five

---
+++ 05_iteration/five.js
@@ -0,0 +1,34 @@
+const coding = ["js","ruby","java","python","cpp"]
+
+// coding.forEach(function (val){
+//      console.log(val);
+      
+// })
+    // function printMe(item){
+    //     console.log(item);
+        
+    // } 
+    //  coding.forEach(printMe)
+    // coding.forEach((item,index,arr)=>{
+    //      console.log(item,index,arr);
+         
+
+    // })
+    const mycoding = [
+        {
+            laguagename:"javascript",
+            laguagefilename:"js"
+        },
+        {
+            laguagename:"java",
+            laguagefilename:"java"
+        },
+        {
+            laguagename:"python",
+            laguagefilename:"py"
+        },
+    ]
+     mycoding.forEach((item)=>{
+        console.log(item.laguagename);
+        
+     })
